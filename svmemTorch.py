ad0fcca8933b4b203b3c978367e7bb1da8f4073b
hyunp2 <42776897+hyunp2@users.noreply.github.com>
2022-06-11 21:20:49 -0500

    Update svmemTorch.py

--- svmemTorch.py
+++ svmemTorch.py
@@ -230,7 +230,7 @@ class svmemTorch(sklearn.base.BaseEstimator, sklearn.base.ClusterMixin, sklearn.
         Use this during INFERENCE!"""
 
         if self.fitted:
-            #samples = self._to_tensor(samples, differentiable=True).to(device) #(batch, natoms_, 3)
+            samples = self._to_tensor(samples, differentiable=True).to(device) #(batch, natoms_, 3)
             print(samples.device)
             assert samples.device.type[:4] == "cuda", "must be on GPU" #WIP 
             batch_size = samples.size(0)
