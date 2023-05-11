d230e7407725c758ec097159a72ff1be78342b62
hyunp2 <42776897+hyunp2@users.noreply.github.com>
2023-05-11 14:58:53 -0500

    Update SVMem_jax.py

--- jax_utils/SVMem_jax.py
+++ jax_utils/SVMem_jax.py
@@ -46,13 +46,13 @@ def vec_mag(vec):
     l = 0.
     for i in range(n):
         l += (vec[i])**2.
-    return np.sqrt(l)
+    return jnp.sqrt(l)
 
 @jit
 def vec_mags(vecs):
     n = vecs.shape[0]
     d = vecs.shape[1]
-    mags = np.empty((n))
+    mags = jnp.empty((n))
     for i in range(n):
         mags[i] = vec_mag(vecs[i])
     return mags
@@ -64,7 +64,7 @@ def vec_norm(vec):
 @jit
 def vec_norms(vecs):
     n = len(vecs)
-    norm_vecs = np.empty_like(vecs)
+    norm_vecs = jnp.empty_like(vecs)
     for i in range(n):
         norm_vecs[i] = vec_norm(vecs[i])
     return norm_vecs
@@ -73,7 +73,7 @@ def vec_norms(vecs):
 def vec_sum(vecs):
     n = vecs.shape[0]
     d = vecs.shape[1]
-    vecsum = np.zeros((d))
+    vecsum = jnp.zeros((d))
     for i in range(n):
         for j in range(d):
             vecsum[j] += vecs[i,j]
@@ -81,7 +81,7 @@ def vec_sum(vecs):
 
 @jit
 def unravel_index(n1, n2):
-    a, b = np.empty((n1, n2), dtype=np.int64), np.empty((n1, n2), dtype=np.int64)
+    a, b = jnp.empty((n1, n2), dtype=jnp.int64), jnp.empty((n1, n2), dtype=jnp.int64)
     for i in range(n1):
         for j in range(n2):
             a[i,j], b[i,j] = i, j
@@ -90,7 +90,7 @@ def unravel_index(n1, n2):
 @jit
 def unravel_upper_triangle_index(n):
     n_unique = (n * (n-1)) // 2
-    a, b = np.empty((n_unique),dtype=np.int64), np.empty((n_unique),dtype=np.int64)
+    a, b = jnp.empty((n_unique),dtype=jnp.int64), jnp.empty((n_unique),dtype=jnp.int64)
     k = 0
     for i in range(n):
         for j in range(n):
@@ -105,21 +105,21 @@ def sym_dist_mat_(xyzs, box_dims, periodic):
     n_unique = (n * (n-1)) // 2
     ndim = xyzs.shape[1]
     i, j = unravel_upper_triangle_index(n)
-    dist_mat = np.zeros((n_unique))
+    dist_mat = jnp.zeros((n_unique))
     for k in prange(n_unique):
         for ri in prange(ndim):
-            dr = np.abs(xyzs[i[k],ri] - xyzs[j[k],ri])
+            dr = jnp.abs(xyzs[i[k],ri] - xyzs[j[k],ri])
             if periodic[ri] == True:
                 while (dr >  (box_dims[ri]*0.5)):
                     dr -= box_dims[ri]
-            dist_mat[k] += np.square(dr)
-    return np.sqrt(dist_mat)
+            dist_mat[k] += jnp.square(dr)
+    return jnp.sqrt(dist_mat)
 
 @jit
 def sym_dist_mat(xyzs, box_dims, periodic):
     n = xyzs.shape[0]
     dist_mat_flat = sym_dist_mat_(xyzs, box_dims, periodic)
-    dist_mat = np.zeros((n,n))
+    dist_mat = jnp.zeros((n,n))
     k = 0
     for i in range(n):
         for j in range(n):
@@ -134,16 +134,16 @@ def dist_mat_(xyz1, xyz2, box_dims, periodic):
     n1 = xyz1.shape[0]
     n2 = xyz2.shape[0]
     ndim = xyz1.shape[1]
-    dist_mat = np.zeros((n1 * n2))
+    dist_mat = jnp.zeros((n1 * n2))
     i, j = unravel_index(n1, n2)
     for k in prange(n1 * n2):
-        dr = np.abs(xyz1[i[k]] - xyz2[j[k]])
+        dr = jnp.abs(xyz1[i[k]] - xyz2[j[k]])
         for ri in range(ndim):
             if periodic[ri] == True:
                 while (dr[ri] >  (box_dims[ri]*0.5)):
                     dr[ri] -= box_dims[ri]
-            dist_mat[k] += np.square(dr[ri])
-    return np.sqrt(dist_mat)
+            dist_mat[k] += jnp.square(dr[ri])
+    return jnp.sqrt(dist_mat)
 
 @jit
 def dist_mat(xyz1, xyz2, box_dims, periodic):
@@ -156,16 +156,16 @@ def dist_mat_parallel_(xyz1, xyz2, box_dims, periodic):
     n1 = xyz1.shape[0]
     n2 = xyz2.shape[0]
     ndim = xyz1.shape[1]
-    dist_mat = np.zeros((n1 * n2))
+    dist_mat = jnp.zeros((n1 * n2))
     i, j = unravel_index(n1, n2)
     for k in prange(n1 * n2):
         for ri in prange(ndim):
-            dr = np.abs(xyz1[i[k],ri] - xyz2[j[k],ri])
+            dr = jnp.abs(xyz1[i[k],ri] - xyz2[j[k],ri])
             if periodic[ri] == True:
                 if (dr >  (box_dims[ri]*0.5)):
                     dr -= box_dims[ri]
-            dist_mat[k] += np.square(dr)
-    return np.sqrt(dist_mat)
+            dist_mat[k] += jnp.square(dr)
+    return jnp.sqrt(dist_mat)
 
 @jit
 def dist_mat_parallel(xyz1, xyz2, box_dims, periodic):
@@ -177,20 +177,20 @@ def dist_mat_parallel(xyz1, xyz2, box_dims, periodic):
 def dist_vec(xyz, xyzs, box_dims, periodic):
     n = len(xyzs)
     ndim = len(xyz)
-    dist_vec = np.zeros((n))
+    dist_vec = jnp.zeros((n))
     for i in prange(n):
         for ri in prange(ndim):
-            dr = np.abs(xyzs[i,ri] - xyz[ri])
+            dr = jnp.abs(xyzs[i,ri] - xyz[ri])
             if periodic[ri] == True:
                 while (dr >  (box_dims[ri]*0.5)):
                     dr -= box_dims[ri]
-            dist_vec[i] += np.square(dr)
-    return np.sqrt(dist_vec)
+            dist_vec[i] += jnp.square(dr)
+    return jnp.sqrt(dist_vec)
 
 @jit
 def disp(xyz1, xyz2, box_dims, periodic):
     d = len(xyz1)
-    disp = np.zeros((d))
+    disp = jnp.zeros((d))
     for ri in range(d):
         dr = xyz1[ri] - xyz2[ri]
         if periodic[ri] == True:
@@ -205,33 +205,33 @@ def disp(xyz1, xyz2, box_dims, periodic):
 def disp_vec(xyz, xyzs, box_dims, periodic):
     n = xyzs.shape[0]
     d = xyzs.shape[1]
-    disps = np.empty((n,d))
+    disps = jnp.empty((n,d))
     for i in range(n):
         disps[i] = disp(xyz, xyzs[i], box_dims, periodic)
     return disps        
 
 @jit
 def gaussian_transform_vec(array, gamma):
-    g_array = np.empty_like(array)
+    g_array = jnp.empty_like(array)
     n = array.shape[0]
     for i in range(n):
-        g_array[i] = np.exp(-gamma * np.square(array[i]))
+        g_array[i] = jnp.exp(-gamma * jnp.square(array[i]))
     return g_array
 
 @njit(parallel=True)
 def gaussian_transform_vec_parallel(array, gamma):
-    g_array = np.empty_like(array)
+    g_array = jnp.empty_like(array)
     n = array.shape[0]
     for i in prange(n):
-        g_array[i] = np.exp(-gamma * np.square(array[i]))
+        g_array[i] = jnp.exp(-gamma * jnp.square(array[i]))
     return g_array
 
 @njit(parallel=True)
 def gaussian_transform_mat_(array, gamma):
-    g_array = np.empty_like(array)
+    g_array = jnp.empty_like(array)
     n = array.shape[0]
     for i in prange(n):
-        g_array[i] = np.exp(-gamma * np.square(array[i]))
+        g_array[i] = jnp.exp(-gamma * jnp.square(array[i]))
     return g_array
 
 @jit
@@ -249,42 +249,42 @@ def decision_function(vec, weights, intercept):
 @jit
 def decision_function_mat(mat, weights, intercept):
     n = mat.shape[0]
-    decisions = np.zeros((n))
+    decisions = jnp.zeros((n))
     for i in range(n):
         decisions[i] = decision_function(mat[i], weights, intercept)
     return decisions
 
 @njit
 def predict(vec, weights, intercept):
-    return np.sign(decision_function(vec, weights, intercept))
+    return jnp.sign(decision_function(vec, weights, intercept))
 
 @jit
 def predict_mat(vec, weights, intercept):
-    return np.sign(decision_function_mat(vec, weights, intercept))
+    return jnp.sign(decision_function_mat(vec, weights, intercept))
 
 @njit(parallel=True)
 def pbc_center(xyzs, box_dims):
     n = xyzs.shape[0]
     d = xyzs.shape[1]
-    center = np.empty((d))
+    center = jnp.empty((d))
     for ri in prange(d):
-        rmax = np.max(xyzs[:,ri])
+        rmax = jnp.max(xyzs[:,ri])
         xi = 0.
         zeta = 0.
         for j in prange(n):
-            thetai = 2.*np.pi*xyzs[j,ri]/rmax
-            xi += np.cos(thetai)
-            zeta += np.sin(thetai)
+            thetai = 2.*jnp.pi*xyzs[j,ri]/rmax
+            xi += jnp.cos(thetai)
+            zeta += jnp.sin(thetai)
         xi /= -n
         zeta /= -n
-        theta = np.arctan2(zeta,xi) + np.pi
-        center[ri] = theta * rmax / (2.*np.pi)
+        theta = jnp.arctan2(zeta,xi) + jnp.pi
+        center[ri] = theta * rmax / (2.*jnp.pi)
     return center
 
 @jit
 def calculate_lipid_coms(lipids_xyz, atom_ids_per_lipid, box_dims):
     n_lipids = len(atom_ids_per_lipid)
-    coms = np.empty((n_lipids, 3))
+    coms = jnp.empty((n_lipids, 3))
     for i in range(n_lipids):
         coms[i] = pbc_center(lipids_xyz[atom_ids_per_lipid[i]], box_dims)
     return coms
@@ -306,7 +306,7 @@ def update_disps(disps, step, box_dims, periodic):
 @jit
 def gradient(disps, gxdists, gamma, weights):
     n = disps.shape[0]
-    del_F = np.zeros((3))
+    del_F = jnp.zeros((3))
     factor = -2.*gamma
     for i in range(n):
         for j in range(3):
@@ -348,7 +348,7 @@ def coordinate_descent(point_, step, disps, box_dims, periodic, weights, interce
         news = nsign_int(d)
         if news != s:
             step *= -0.5
-        if np.abs(d) < tol:
+        if jnp.abs(d) < tol:
             break
         s = news
     return point
@@ -357,8 +357,8 @@ def coordinate_descent(point_, step, disps, box_dims, periodic, weights, interce
 def descend_to_boundary(points, support_points, box_dims, periodic, weights, intercept, gamma, learning_rate, max_iter, tol):
     n = points.shape[0]
     d = points.shape[1]
-    bounds = np.empty((n, d))
-    normal_vectors = np.empty((n, d))
+    bounds = jnp.empty((n, d))
+    normal_vectors = jnp.empty((n, d))
     for i in prange(n):
         approx_bound, normal_vectors[i], disps  = gradient_descent(
             points[i], support_points, 
@@ -377,22 +377,22 @@ def analytical_derivative(point, support_points, box_dims, periodic, gamma, weig
     disps = disp_vec(point, support_points, box_dims, periodic)
     gxdists = gaussian_transform_vec(vec_mags(disps), gamma)
     grad = -2. * gamma * disps * gxdists.reshape(-1,1) * weights.reshape(-1,1)
-    hess = np.zeros((d,d))
+    hess = jnp.zeros((d,d))
     for i in range(d):
         for j in range(d):
             if i < j:
-                hess[i,j] = np.sum(-2. * gamma * disps[:,j] * grad[:,i])
+                hess[i,j] = jnp.sum(-2. * gamma * disps[:,j] * grad[:,i])
     for i in range(d):
-        hess[i,i] = np.sum(-2. * gamma * (1. - 2. * gamma * np.square(disps[:,i])) * gxdists * weights)
+        hess[i,i] = jnp.sum(-2. * gamma * (1. - 2. * gamma * jnp.square(disps[:,i])) * gxdists * weights)
     for i in range(d):
         for j in range(i+1,d):
             hess[j,i] = hess[i,j]
-    return np.sum(grad,axis=0), hess
+    return jnp.sum(grad,axis=0), hess
 
 @jit
 def gaussian_curvature(grad, hess):
     n = len(grad)
-    X = np.empty((n+1, n+1))
+    X = jnp.empty((n+1, n+1))
     X[n,n] = 0.
     for i in range(n):
         for j in range(n):
@@ -401,19 +401,19 @@ def gaussian_curvature(grad, hess):
         X[n,i] = grad[i]
         X[i,n] = grad[i]
     div = -(vec_mag(grad)**4.)
-    return np.linalg.det(X) / div   
+    return jnp.linalg.det(X) / div   
 
 @jit
 def mean_curvature(grad, hess):
     grad_mag = vec_mag(grad)
     div = 2. * (grad_mag**3.)
-    return ((grad @ hess @ grad.T) + (-(grad_mag**2.) * np.trace(hess))) / div
+    return ((grad @ hess @ grad.T) + (-(grad_mag**2.) * jnp.trace(hess))) / div
 
 @njit(parallel=True)
 def curvatures(points, support_points, box_dims, periodic, gamma, weights):
     n = points.shape[0]
-    mean_curvatures = np.zeros((n))
-    gaussian_curvatures = np.zeros((n))
+    mean_curvatures = jnp.zeros((n))
+    gaussian_curvatures = jnp.zeros((n))
     for i in prange(n):
         grad, hess = analytical_derivative(points[i], support_points, box_dims, periodic, gamma, weights)
         gaussian_curvatures[i] = gaussian_curvature(grad, hess)
@@ -430,11 +430,11 @@ class SVMem_jax(object):
         elif len(train_indices.shape) > 1:
             raise ValueError('training indices (train_indices) must be a one-dimensional array of integers (%i-d supplied)'%len(train_indices.shape))
         else:
-            self.xyz = xyz.astype(np.float64)
-            self.box_dims = box_dims.astype(np.float64)
+            self.xyz = xyz.astype(jnp.float64)
+            self.box_dims = box_dims.astype(jnp.float64)
             self.periodic = periodic
             self.gamma = gamma
-            self.train_indices = train_indices.astype(np.int64)
+            self.train_indices = train_indices.astype(jnp.int64)
             self.train_points = xyz[:,train_indices].copy()
             self.n_train_points = len(train_indices)
             self.train_labels = None
@@ -464,7 +464,7 @@ class SVMem_jax(object):
         else:
             self.tol = 0.0001 # default tol
         if train_labels != 'auto':
-            if len(np.where(train_labels == 1.)[0]) + len(np.where(train_labels == -1.)[0]) != self.n_train_points:
+            if len(jnp.where(train_labels == 1.)[0]) + len(jnp.where(train_labels == -1.)[0]) != self.n_train_points:
                 raise ValueError('supplied training labels (train_labels) must be either -1.0 (for bottom leaflet) or 1.0 (for top leaflet)')
             self.train_labels = train_labels
             self.autogenerate_labels = False
@@ -474,17 +474,17 @@ class SVMem_jax(object):
     def _calculate_train_labels(self, frame, distance_matrix):
         cluster = AgglomerativeClustering(n_clusters=2, affinity='precomputed', linkage='single')
         self.train_labels = cluster.fit_predict(distance_matrix)
-        zmean0 = np.mean(self.train_points[frame,:,2][np.where(self.train_labels==0)[0]])
-        zmean1 = np.mean(self.train_points[frame,:,2][np.where(self.train_labels==1)[0]])
+        zmean0 = jnp.mean(self.train_points[frame,:,2][jnp.where(self.train_labels==0)[0]])
+        zmean1 = jnp.mean(self.train_points[frame,:,2][jnp.where(self.train_labels==1)[0]])
         if zmean0 > zmean1:
-            self.train_labels = np.where(self.train_labels==0, 1., -1.)
+            self.train_labels = jnp.where(self.train_labels==0, 1., -1.)
         else:
-            self.train_labels = np.where(self.train_labels==1, 1., -1.)
+            self.train_labels = jnp.where(self.train_labels==1, 1., -1.)
     
     def _train_svm(self, kernel_matrix):
         model = SVC(C=1.,kernel='precomputed', verbose=0, cache_size=1000)
         model.fit(X=kernel_matrix, y=self.train_labels)
-        return model.dual_coef_.astype(np.float64).flatten(), model.intercept_.astype(np.float64)[0], model.support_.astype(np.int64).flatten()
+        return model.dual_coef_.astype(jnp.float64).flatten(), model.intercept_.astype(jnp.float64)[0], model.support_.astype(jnp.int64).flatten()
     
     def _calculate_curvature(self, frame):
         train_mat = sym_dist_mat(self.train_points[frame], self.box_dims[frame], self.periodic)
@@ -506,11 +506,11 @@ class SVMem_jax(object):
         if (frames is None and stride is None) or (frames is not None and stride is not None):
             raise ValueError('Please supply only one of the parameters frames or stride')
         elif frames is None and stride is not None:
-            frames = np.arange(0, len(self.xyz), stride)
+            frames = jnp.arange(0, len(self.xyz), stride)
         elif frames is not None and stride is None:
             if type(frames) == str:
                 if frames == 'all':
-                    frames = np.arange(0, len(self.xyz))
+                    frames = jnp.arange(0, len(self.xyz))
             else:
                 try:
                     assert len(frames.shape) == 1
@@ -520,9 +520,9 @@ class SVMem_jax(object):
         self.weights_list = []
         self.intercept_list = []
         self.support_indices_list = []
-        self.normal_vectors = np.empty((n_frames, self.n_train_points, 3))
-        self.mean_curvature = np.empty((n_frames, self.n_train_points))
-        self.gaussian_curvature = np.empty((n_frames, self.n_train_points))
+        self.normal_vectors = jnp.empty((n_frames, self.n_train_points, 3))
+        self.mean_curvature = jnp.empty((n_frames, self.n_train_points))
+        self.gaussian_curvature = jnp.empty((n_frames, self.n_train_points))
         for i,frame in enumerate(frames):
             self.mean_curvature[i], self.gaussian_curvature[i], self.normal_vectors[i], weights_i, intercept_i, support_indices_i = self._calculate_curvature(frame)
             self.weights_list.append(weights_i)
@@ -544,10 +544,10 @@ if __name__ == "__main__":
     head_selection = lipid.top.select(head_selection_text)
 
     # define periodicity of system in x,y,z directions
-    periodic = np.array([True, True, False]) 
+    periodic = jnp.array([True, True, False]) 
 
     # get indices of each lipid, required for COM calculation
-    atom_ids_per_lipid = [np.array([atom.index for atom in residue.atoms]) for residue in lipid.top.residues] 
+    atom_ids_per_lipid = [jnp.array([atom.index for atom in residue.atoms]) for residue in lipid.top.residues] 
 
     # define gamma, hyperparameter used for RBF kernel 
     gamma = 0.1 
