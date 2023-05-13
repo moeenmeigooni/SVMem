c12b2b5f8269f716084784fd71aec96910a042e4
hyunp2 <42776897+hyunp2@users.noreply.github.com>
2023-05-12 22:43:30 -0500

    Update SVMem_jax.py

--- jax_utils/SVMem_jax.py
+++ jax_utils/SVMem_jax.py
@@ -580,9 +580,13 @@ if __name__ == "__main__":
 
     
     ########TEST########
+    xyzs = jnp.array(np.random.normal(size=(20,3)))
+
+    r = vec_sum(xyzs)
+    print(r)
+    
     periodic = jnp.array(periodic)
     box_dims=jnp.array([4,3,5])
-    xyzs = jnp.array(np.random.normal(size=(20,3)))
     r = sym_dist_mat(xyzs, box_dims, periodic)       
     print(r)
     
